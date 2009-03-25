fae92677e4f4951beebf98e9f1484695b0f4b6a6
Glen E. Ivey <gleneivey@wontology.org>
2009-03-25 15:44:49 -0700

    Upgraded to Cucumber v0.2.2

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@
 
 # Sets up the Rails environment for Cucumber
 ENV["RAILS_ENV"] = "test"
-$KCODE = "U"
+$KCODE = "u"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
 require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
@@ -23,5 +23,5 @@ end
 
 # Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
-require 'webrat/rspec-rails'
+#require 'webrat/rspec-rails'
 #require 'webrat/core/matchers'
