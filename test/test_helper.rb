2cafa834f777348339fbc8eb7f69d2b4a8fa96e9
Glen E. Ivey <gleneivey@wontology.org>
2009-04-06 12:22:33 -0700

    Cleanup of require path creation in test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,7 @@
 
 
 ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require File.join( File.dirname(__FILE__), "..", "config", "environment" )
 require 'test_help'
 
 class ActiveSupport::TestCase
