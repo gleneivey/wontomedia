dcec2f97b6036c34bca1605b564c38e820a72bfc
Glen E. Ivey <gleneivey@wontology.org>
2009-04-07 14:45:46 -0700

    Created the YAML files used to initialize the Nodes and Edges database tables with the built-in root ontology schema.  Had to create a minimal implementation of the Edge model (four belongs_to associations) in order for Fixtures to be able to correctly parse/load this YAML.  This is not the "real" Edge implementation, and there are no unit tests yet (apart from the YAML files themselves, which cause "rake default" to fail if edge.rb is missing).

---
+++ app/models/edge.rb
@@ -0,0 +1,25 @@
+# WontoMedia - a wontology web application
+# Copyright (C) 2009 - Glen E. Ivey
+#    www.wontology.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version
+# 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in the file COPYING and/or LICENSE.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+
+class Edge < ActiveRecord::Base
+  belongs_to :subject,   :class_name => "Node"
+  belongs_to :predicate, :class_name => "Node"
+  belongs_to :object,    :class_name => "Node"
+  belongs_to :self,      :class_name => "Node"
+
+end
