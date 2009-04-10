2dcc9d8f52504c9bf6476a80941ea155a3539389
Glen E. Ivey <gleneivey@wontology.org>
2009-04-10 10:42:02 -0700

    features/support/env.rb (Cucumber config file): cleaned up comments, spacing.

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,12 +16,8 @@ require 'cucumber/formatters/unicode' # Comment out this line if you don't want 
 Cucumber::Rails.use_transactional_fixtures
 
 require 'webrat'
-
 Webrat.configure do |config|
   config.mode = :rails
 end
 
-# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
-#require 'webrat/rspec-rails'
-#require 'webrat/core/matchers'
