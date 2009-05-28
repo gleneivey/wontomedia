d5386859f5c7ede305c208c8c455a91ca94e6367
Glen E. Ivey <gleneivey@wontology.org>
2009-05-28 11:17:43 -0700

    Changed sed regular expression for A2-specific change to environment.rb. This version works better....

--- deploy/wontology-on-a2hosting/install-wontomedia.sh
+++ deploy/wontology-on-a2hosting/install-wontomedia.sh
@@ -5,7 +5,7 @@ gem install -l wontomedia
 ln -s /home/glenivey/ruby/gems/gems/wontomed* /home/glenivey/etc/rails_apps/WontoMedia
 
 cp /home/glenivey/wm.database.yml /home/glenivey/etc/rails_apps/WontoMedia/config/database.yml
-sed --in-place=.backup -e '/^RAILS_GEM_.+VERSION/aGem.use_paths "/home/glenivey/ruby/gems", [ "/home/glenivey/ruby/gems", "/usr/lib/ruby/gems/1.8" ]' /home/glenivey/etc/rails_apps/WontoMedia/config/environment.rb
+sed --in-place=.backup -e '/^RAILS_GEM_VERSION/aGem.use_paths "/home/glenivey/ruby/gems", [ "/home/glenivey/ruby/gems", "/usr/lib/ruby/gems/1.8" ]' /home/glenivey/etc/rails_apps/WontoMedia/config/environment.rb
 
 mkdir /home/glenivey/etc/rails_apps/WontoMedia/log
 mkdir /home/glenivey/etc/rails_apps/WontoMedia/tmp
