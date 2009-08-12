c4fdf5b3bea3da73f175e1bbd4533b37cc061e6a
Glen E. Ivey <gleneivey@wontology.org>
2009-08-12 10:22:27 -0700

    Another better-TEXTAREA-support update to env.js.

--- vendor/plugins/blue-ridge/lib/env.rhino.js
+++ vendor/plugins/blue-ridge/lib/env.rhino.js
@@ -38,10 +38,10 @@ var Envjs = function(){
     //uncomment these if you want to get some internal log statementes
     /*$env.debug  = function(msg){
         $env.log(msg,"DEBUG"); 
-    };*/
+    };
     $env.info   = function(msg){
         $env.log(msg,"INFO"); 
-    };
+    };*/
     $env.warn   = function(msg){
         $env.log(msg,"WARNIING");    
     };
@@ -5392,7 +5392,7 @@ __extend__(HTMLDocument.prototype, {
           // create DOMElement specifying 'this' as ownerDocument
           //This is an html document so we need to use explicit interfaces per the 
           if(     tagName.match(/^A$/))                 {node = new HTMLAnchorElement(this);}
-          else if(tagName.match(/AREA/))                {node = new HTMLAreaElement(this);}
+          else if(tagName.match(/^AREA$/))              {node = new HTMLAreaElement(this);}
           else if(tagName.match(/BASE/))                {node = new HTMLBaseElement(this);}
           else if(tagName.match(/BLOCKQUOTE|Q/))        {node = new HTMLQuoteElement(this);}
           else if(tagName.match(/BODY/))                {node = new HTMLBodyElement(this);}
@@ -7799,7 +7799,118 @@ __extend__(HTMLTableCellElement.prototype, {
     
 });
 
-$w.HTMLTableCellElement	= HTMLTableCellElement;$debug("Defining HTMLTitleElement");
+$w.HTMLTableCellElement	= HTMLTableCellElement;$debug("Defining HTMLTextAreaElement");
+/*
+* HTMLTextAreaElement - DOM Level 2
+*/
+var HTMLTextAreaElement = function(ownerDocument) {
+    this.HTMLElement = HTMLElement;
+    this.HTMLElement(ownerDocument);
+};
+HTMLTextAreaElement.prototype = new HTMLElement;
+__extend__(HTMLTextAreaElement.prototype, {
+    get cols(){
+        return this.getAttribute('cols');
+    },
+    set cols(value){
+        this.setAttribute('cols', value);
+    },
+    get rows(){
+        return this.getAttribute('rows');
+    },
+    set rows(value){
+        this.setAttribute('rows', value);
+    },
+
+    get defaultValue(){
+        return this.getAttribute('defaultValue');
+    },
+    set defaultValue(value){
+        this.setAttribute('defaultValue', value);
+    },
+    get form(){
+        var parent = this.parent;
+        while(parent.nodeName.toLowerCase() != 'form'){
+            parent = parent.parent;
+        }
+        return parent;
+    },
+    get accessKey(){
+        return this.getAttribute('accesskey');
+    },
+    set accessKey(value){
+        this.setAttribute('accesskey',value);
+    },
+    get access(){
+        return this.getAttribute('access');
+    },
+    set access(value){
+        this.setAttribute('access', value);
+    },
+    get disabled(){
+        return (this.getAttribute('disabled')=='disabled');
+    },
+    set disabled(value){
+        this.setAttribute('disabled', (value ? 'disabled' :''));
+    },
+    get name(){
+        return this.getAttribute('name')||'';
+    },
+    set name(value){
+        this.setAttribute('name', value);
+    },
+    get readOnly(){
+        return (this.getAttribute('readonly')=='readonly');
+    },
+    set readOnly(value){
+        this.setAttribute('readonly', (value ? 'readonly' :''));
+    },
+    get tabIndex(){
+        return Number(this.getAttribute('tabindex'));
+    },
+    set tabIndex(value){
+        this.setAttribute('tabindex',Number(value));
+    },
+    get type(){
+        return this.getAttribute('type');
+    },
+    set type(value){
+        this.setAttribute('type',value);
+    },
+    get value(){
+        return this.getAttribute('value');
+    },
+    set value(value){
+        this.setAttribute('value',value);
+    },
+    blur:function(){
+        __blur__(this);
+
+        if (this._oldValue != this.getAttribute('value')){
+            var event = document.createEvent();
+            event.initEvent("change");
+            this.dispatchEvent( event );
+        }
+    },
+    focus:function(){
+        __focus__(this);
+        this._oldValue = this.getAttribute('value');
+    },
+    select:function(){
+        __select__(this);
+
+    },
+    click:function(){
+        __click__(this);
+
+    },
+    onchange: function(event){
+        __eval__(this.getAttribute('onchange')||'', this)
+    }
+});
+
+$w.HTMLTextAreaElement = HTMLTextAreaElement;
+$debug("Defining HTMLTitleElement");
 /* 
 * HTMLTitleElement - DOM Level 2
 */
