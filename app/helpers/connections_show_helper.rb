7ab545b33e2e02891c9bb06e4a052188d63afb9e
Glen E. Ivey <gleneivey@wontology.org>
2010-02-24 21:34:53 -0800

    Adding RDoc comments to helpers.  Made some cosmetic source changes.  Most files changed due to rearranging of the parameters to FormatHelper.popup_help_icon (middle argument now the final argument and optional).

--- app/helpers/connections_show_helper.rb
+++ app/helpers/connections_show_helper.rb
@@ -16,22 +16,33 @@
 # see <http://www.gnu.org/licenses/>.
 
 
+# Helpers for app/views/connections/show
 module ConnectionsShowHelper
-  def generate_connection_links(e)
+
+  # This method appends its output directly to the page being built.
+  # It takes a Connection model instance, and generates a set of HTML
+  # links to that connection.  It generates a "Show" link to the
+  # Connection's <tt>connections/show</tt> page.  Unless the
+  # Connection is flagged as <tt>DATA_IS_UNALTERABLE</tt>, it also
+  # generates an "Edit..." link to +con+'s <tt>connections/edit</tt>
+  # page and a "Delete" link that will invoke
+  # <tt>connections/destroy</tt>.  All of the links have a "help" icon
+  # and popup, see FormatHelper.a_help_link().
+  def generate_connection_links(con)
     @show_help_icon_used, fragment =
-      a_help_link( '', link_to( 'Show', connection_path(e) ),
+      a_help_link( '', link_to( 'Show', connection_path(con) ),
         @show_help_icon_used, 'Help show connection', 'ConnectionShow' )
     concat( fragment )
 
-    if (e.flags & Connection::DATA_IS_UNALTERABLE) == 0
+    if (con.flags & Connection::DATA_IS_UNALTERABLE) == 0
       @edit_help_icon_used, fragment =
-        a_help_link( '', link_to( 'Edit&hellip;', edit_connection_path(e),
+        a_help_link( '', link_to( 'Edit&hellip;', edit_connection_path(con),
             :rel => 'nofollow' ),
           @edit_help_icon_used, 'Help edit connection', 'ConnectionEdit' )
       concat( fragment )
 
       @delete_help_icon_used, fragment =
-        a_help_link( '', link_to( 'Delete', connection_path(e),
+        a_help_link( '', link_to( 'Delete', connection_path(con),
             :rel => 'nofollow', :confirm => 'Are you sure?',
             :method => :delete ),
           @delete_help_icon_used, 'Help delete connection', 'ConnectionDelete' )
--- lib/helpers/tripple_navigation.rb
+++ lib/helpers/tripple_navigation.rb
@@ -16,7 +16,22 @@
 # see <http://www.gnu.org/licenses/>.
 
 
+# This module contains methods for navigating through (walking) the
+# inheritence hierarchy between the properties of connections.  They
+# allow callers to make queries about a property, and receive answers
+# that take into account the 'sub_property_of' relationships between
+# the immediately queried property and the property inheritance tree.
 module TrippleNavigation
+
+  # This method finds all existing PropertyItem's that are super
+  # properties of the PropertyItem identified by +predicate_id+.  The
+  # immediate super properties of an item are the objects of any
+  # connection of the form "item sub_property_of object".  The set of
+  # super properties found by this method includes immediate super
+  # properties and all of _their_ super properties.  This method
+  # accepts a block, and iteratively yields _its original argument_
+  # and the _IDs_ of the argument property's super properties to that
+  # block.
   def self.relation_and_all_superproperties(predicate_id, &block)
     yield predicate_id
 
@@ -28,6 +43,14 @@ module TrippleNavigation
     end
   end
 
+  # This method finds all existing PropertyItem's that are sub
+  # properties of the PropertyItem identified by +predicate_id+.  The
+  # immediate sub properties of an item are the subjects of any
+  # connection of the form "subject sub_property_of item".  The set of
+  # sub properties found by this method includes immediate sub
+  # properties and all of _their_ sub properties.  This method accepts
+  # a block, and iteratively yields _its original argument_ and the
+  # _IDs_ of the argument property's sub properties to that block.
   def self.relation_and_all_subproperties(predicate_id, &block)
     yield predicate_id
 
@@ -39,12 +62,41 @@ module TrippleNavigation
     end
   end
 
+  # This method tests for chains of relationships between
+  # PropertyItem's.  Its parameters are packaged in a single hash
+  # argument.  The value of each entry in the hash is the _ID_ of an
+  # Item in the database.  It returns a boolean that answers the
+  # "question" phrased in the parameter hash.
+  #
+  # Currently, there are two supported combinations of argument
+  # symbols:
+  #
+  # * <tt>:does</tt> xxxx <tt>:inherit_from</tt> xxxx <tt>:via</tt> xxxx
+  # * <tt>:does</tt> xxxx <tt>:link_to</tt> xxxx <tt>:through_children_of</tt> xxxx
+  #
+  # Both of these combinations form a question, the answer to which is
+  # in the boolean returned by this method (+true+ for yes).  The
+  # symbols used to index the argument hash are:
+  #
+  # [:does] The value in the hash associated with <tt>:does</tt> is the ID of the Item for which the caller is querying the connections stored in the database.
+  # [:inherit_from] The value in the hash associated with <tt>:inherit_from</tt> is the ID of a PropertyItem (different from the <tt>:does</tt> Item) in the database.  check_properties() will test for the presence of specific connections between this PropertyItem and the one specified by <tt>:does</tt>.
+  # [:via] The value in the hash associated with <tt>:via</tt> is the ID of the PropertyItem that must be present as the predicate of a connection for that connection to "count" toward establishing a chain of connections from the <tt>:does</tt> item to the <tt>:inherit_from</tt> item.  Note that <tt>:via</tt> is directional:  When <tt>:inherit_from</tt> is used, only connections where the <tt>:does</tt> item is the subject, the <tt>:inherit_from</tt> is the object, and all intervening connections "point" in that same direction will be tested, and this method will return <tt>true</tt> only when a chain of connections using the <tt>:via</tt> PropertyItem as their predicates can be found.
+  # [:link_to] The value in the hash associated with <tt>:link_to</tt> is the ID of another Item, to which check_properties will try to find a chain of connections from the <tt>:does</tt> Item.  Note that unlike calls to check_properties using <tt>:inherit_from</tt>, when <tt>:link_to</tt> is used the starting and ending items can be of any type, not just PropertyItems.
+  # [:through_children_of] The value in the hash associated with <tt>:through_children_of</tt> is the ID of a PropertyItem.  check_properties attempts to find a chain of connections between the other two Item parameters (in either direction) where the connections use the <tt>:through_children_of</tt> PropertyItem _or_ any PropertyItem that is a _child_ of the specified PropertyItem through the built-in <tt>sub_property_of</tt> relationship.  Unlike calls using <tt>:inherit_from</tt>, when checking <tt>:link_to</tt> connections present in either direction between the items found in a chain can produce a <tt>true</tt> result.
   def self.check_properties(hash_of_params)
     unless hash_of_params.has_key?(:does)
       raise ArgumentError, "Expected :does in input hash"
     end
 
     if hash_of_params.has_key?(:inherit_from)
+      # TODO: ':via' is *almost* always a reference to
+      # sub_property_of.  Change this method and refactor callers so
+      # that when :via isn't present, it defaults to s_p_o.  (If we
+      # keep asking ActiveRecord to find_by_name(s_p_o), will it be
+      # cached or a huge performance hit?  Experiment and if
+      # A.R. really goes to the database each time, have a static
+      # variable here so that we only have to fetch from the db the
+      # first time.)
       unless hash_of_params.has_key?(:via)
         raise ArgumentError, "Expected :via in input hash"
       end
@@ -60,9 +112,10 @@ module TrippleNavigation
       connections = Connection.all( :conditions => [
         "subject_id = ? AND predicate_id = ?", child_id, via_property_id ])
       connections.each do |e|
-        if check_properties( :does         => e.obj_id,
-                             :inherit_from => parent_id,
-                             :via          => via_property_id )
+        if check_properties(
+            :does         => e.obj_id,
+            :inherit_from => parent_id,
+            :via          => via_property_id )
           return true
         end
       end
@@ -79,15 +132,17 @@ module TrippleNavigation
       connections = Connection.all( :conditions =>
         [ "subject_id = ?", from_item_id ])
       connections.each do |e|
-        if check_properties( :does         => e.predicate_id,
-                             :inherit_from => kind_of_path,
-                             :via          => spo_id )
+        if check_properties(
+            :does         => e.predicate_id,
+            :inherit_from => kind_of_path,
+            :via          => spo_id )
           if e.obj_id == to_item_id
             return true
           else
-            if check_properties( :does                => e.obj_id,
-                                 :link_to             => to_item_id,
-                                 :through_children_of => kind_of_path )
+            if check_properties(
+                :does                => e.obj_id,
+                :link_to             => to_item_id,
+                :through_children_of => kind_of_path )
               return true
             end
           end
