920475560d58c8d0b674fb86439dbb00bc45c1f9
Glen E. Ivey <gleneivey@wontology.org>
2010-03-21 20:04:13 -0700

    Added a helper method to compare two connections, intended to provide a user- friendly sort order for the first connection list in an items/show page.

--- lib/helpers/connection_helper.rb
+++ lib/helpers/connection_helper.rb
@@ -42,4 +42,82 @@ module ConnectionHelper
     end
     result
   end
+
+  # An operator usable for sorting, returns 0 for a==b, <0 for a<b
+  def self.compare(a, b, factor_hash = {})
+    # sort relationships using built-in predicates to the bottom
+    aflag = a.predicate.flags & Item::DATA_IS_UNALTERABLE
+    bflag = b.predicate.flags & Item::DATA_IS_UNALTERABLE
+    return aflag - bflag if aflag != bflag
+
+      # sort relationships to scalar values above those with item objects
+    # connection with filled-in scalar valued object
+    if a.kind_of_obj && b.kind_of_obj && a.kind_of_obj != b.kind_of_obj
+      return a.kind_of_obj == Connection::OBJECT_KIND_ITEM ? 1 : -1
+    end
+    # connection with blank object intended to be a scalar
+    a_kind = Connection.first( :conditions => [
+        "subject_id = ? AND predicate_id = ?", a.predicate_id,
+        Item.find_by_name('has_scalar_object') ])
+    b_kind = Connection.first( :conditions => [
+        "subject_id = ? AND predicate_id = ?", b.predicate_id,
+        Item.find_by_name('has_scalar_object') ])
+    if a_kind != b_kind
+      return a_kind.nil? ? 1 : -1
+    end
+
+    # relationships w/ predicates constrained by max_uses_per_item:
+    #   sort lower maximums closer to top of list.  Sort unconstrained
+    #   predicates as "infinite maximum"
+    a_max = Connection.first( :conditions => [
+      "subject_id = ? AND predicate_id = ?", a.predicate_id,
+      Item.find_by_name('max_uses_per_item') ])
+    unless a_max.nil?
+      a_max = a_max.scalar_obj.nil? ? nil : a_max.scalar_obj.to_i
+    end
+    b_max = Connection.first( :conditions => [
+      "subject_id = ? AND predicate_id = ?", b.predicate_id,
+      Item.find_by_name('max_uses_per_item') ])
+    unless b_max.nil?
+      b_max = b_max.scalar_obj.nil? ? nil : b_max.scalar_obj.to_i
+    end
+    unless a_max == b_max
+      return -1 if b_max.nil?
+      return  1 if a_max.nil?
+      return a_max - b_max
+    end
+
+    a_id = a.predicate_id
+    b_id = b.predicate_id
+
+    # sort by "predicate external factor" hash, higher numbers better
+    # (this is intended to support, among other things, sorting based
+    # on the number of times the same predicate is used in the list of
+    # connections being sorted.  Requires caller to make a counting
+    # pass over the sort set first to build the hash
+    a_count = factor_hash[a_id].nil? ? 0 : factor_hash[a_id]
+    b_count = factor_hash[b_id].nil? ? 0 : factor_hash[b_id]
+    return b_count - a_count if a_count != b_count
+
+    # sort by property ID order (this is essentially arbitrary, but
+    # ensures the sort order is stable and that subsequent factors
+    # only affect sorting within groups of connections all referencing
+    # the same property item)
+    return a_id - b_id if a_id != b_id
+
+    # sort connections with missing objects (e.g. "add here"
+    # place-holders) below others
+    a_blank = a.kind_of_obj.nil? ||
+        (a.kind_of_obj == Connection::OBJECT_KIND_ITEM ?
+          a.obj : a.scalar_obj ) == nil
+    b_blank = b.kind_of_obj.nil? ||
+        (b.kind_of_obj == Connection::OBJECT_KIND_ITEM ?
+          b.obj : b.scalar_obj ) == nil
+    return a_blank ? 1 : -1 if a_blank != b_blank
+
+    # sort connections whose objects are built-in below others
+    aflag = a.obj.nil? ? 0 : a.obj.flags & Item::DATA_IS_UNALTERABLE
+    bflag = b.obj.nil? ? 0 : b.obj.flags & Item::DATA_IS_UNALTERABLE
+    return aflag - bflag
+  end
 end
